b40a5a91e8c2c8b13c34961485d5eea74816060d
pmcgrath <pmcgrath@travelrepublic.co.uk>
2015-10-01 09:16:44 +0100

    Added some comments regarding security

--- repomgr/cmd_test.go
+++ repomgr/cmd_test.go
@@ -6,7 +6,24 @@ import (
 	"testing"
 )
 
-func TestExecCmd(t *testing.T) {
+func TestExecCmdForKnownCommand(t *testing.T) {
+	output, err := execCmd("hostname")
+	if err != nil {
+		t.Errorf("Unexpected result for running a known command, error: %#v", err)
+	}
+	if len(output) < 1 || len(output[0]) < 1 {
+		t.Error("Expected a result for running a known command")
+	}
+}
+
+func TestExecCmdForUnknownCommand(t *testing.T) {
+	_, err := execCmd("ted_unknown")
+	if err == nil {
+		t.Error("Unexpected result for running unkown command")
+	}
+}
+
+func TestExecCmdForLs(t *testing.T) {
 	currentDir, _ := os.Getwd()
 	tempDir := os.TempDir()
 	nonExistentDir := path.Join(tempDir, "does_not_exist")
@@ -21,7 +38,6 @@ func TestExecCmd(t *testing.T) {
 	} {
 		_, err := execCmd("ls", "-al", testCase.Path)
 		errEncountered := err != nil
-		t.Logf("---> %s: %t\n", testCase.Path, errEncountered)
 		if errEncountered != testCase.ExpectedError {
 			t.Errorf("Unexpected result for [%s], expected to encounter an error = %t but got an error = %t", testCase.Path, testCase.ExpectedError, errEncountered)
 		}
