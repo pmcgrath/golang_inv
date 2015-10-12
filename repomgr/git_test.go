c4006a16cf39a795d64bf1e5c606a031e0d71882
pmcgrath <pmcgrath@gmail.com>
2015-10-12 23:29:24 +0100

    Refactored tests

--- repomgr/git_test.go
+++ repomgr/git_test.go
@@ -7,35 +7,72 @@ import (
 	"testing"
 )
 
-func TestExecGitBranch(t *testing.T) {
+func TestExecAllDirectGitCommandsExcludingClone(t *testing.T) {
 	// Needed to be in sorted order as the test assertions depend on this
-	repoPaths := make([]string, 100)
+	repoPaths := make([]string, 1)
 	for index := 0; index < len(repoPaths); index++ {
 		repoPaths[index] = fmt.Sprintf("/tmp/repos/repo%03d", index+1)
 	}
+	remoteName := "ARemoteSomewhere"
 
 	previous := cmdExecutionFn
 	defer func() { cmdExecutionFn = previous }()
 	cmdExecutionFn = execCmdSpy
 
-	results := execGitBranch(repoPaths)
+	for _, cmd := range []command{
+		branchCmd,
+		fetchCmd,
+		pullCmd,
+		remoteCmd,
+		statusCmd} {
 
-	if len(results) != len(repoPaths) {
-		t.Errorf("Unexpected result count : %d", len(results))
-	}
-	for index, repoPath := range repoPaths {
-		repoResult := results[index] // Since we ensured the input is sorted, we can rely on the order
+		var cmdSuffix string
+		var results gitCmdResults
+		switch cmd {
+		case branchCmd:
+			// Sample command is : git --git-dir /tmp/repos/repo1/.git --work-tree /tmp/repos/repo1 branch -av
+			// So suffix is "-av"
+			cmdSuffix = "-av"
+			results = execGitBranch(repoPaths)
+		case fetchCmd:
+			// Sample command is : git --git-dir /tmp/repos/repo1/.git --work-tree /tmp/repos/repo1 fetch origin
+			// So suffix is "origin" or whatever remote name we are running against
+			cmdSuffix = remoteName
+			results = execGitFetch(repoPaths, remoteName)
+		case pullCmd:
+			// Sample command is : git --git-dir /tmp/repos/repo1/.git --work-tree /tmp/repos/repo1 pull origin
+			// So suffix is "origin" or whatever remote name we are running against
+			cmdSuffix = remoteName
+			results = execGitPull(repoPaths, remoteName)
+		case remoteCmd:
+			// Sample command is : git --git-dir /tmp/repos/repo1/.git --work-tree /tmp/repos/repo1 remote --verbose
+			// So suffix is "--verbose"
+			cmdSuffix = "--verbose"
+			results = execGitRemote(repoPaths)
+		case statusCmd:
+			// Sample command is : git --git-dir /tmp/repos/repo1/.git --work-tree /tmp/repos/repo1 status --porcelain
+			// So suffix is "--porcelain"
+			cmdSuffix = "--porcelain"
+			results = execGitStatus(repoPaths)
+		}
 
-		if repoResult.RepoPath != repoPath {
-			t.Errorf("Unexpected result RepoPath, expected %s, but got %s", repoPath, repoResult.RepoPath)
+		if len(results) != len(repoPaths) {
+			t.Errorf("Unexpected result count : %d", len(results))
 		}
-		if repoResult.Command != branchCmd {
-			t.Errorf("Unexpected result Command, expected %s, but got %s", branchCmd, repoResult.Command)
-		}
-		// Sample command is : git --git-dir /tmp/repos/repo1/.git --work-tree /tmp/repos/repo1 branch -av
-		expectedOutput := fmt.Sprintf("cmdName: git, args: --git-dir %s/.git --work-tree %s branch -av", repoPath, repoPath)
-		if repoResult.Output[0] != expectedOutput {
-			t.Errorf("Unexpected result Output, expected %s, but got %s", expectedOutput, repoResult.Output[0])
+		for index, repoPath := range repoPaths {
+			repoResult := results[index] // Since we ensured the input is sorted, we can rely on the order
+
+			if repoResult.RepoPath != repoPath {
+				t.Errorf("Unexpected result RepoPath, expected %s, but got %s", repoPath, repoResult.RepoPath)
+			}
+			if repoResult.Command != string(cmd) {
+				t.Errorf("Unexpected result Command, expected %s, but got %s", cmd, repoResult.Command)
+			}
+			// Sample command is : git --git-dir /tmp/repos/repo1/.git --work-tree /tmp/repos/repo1 branch -av
+			expectedOutput := fmt.Sprintf("cmdName: git, args: --git-dir %s/.git --work-tree %s %s %s", repoPath, repoPath, cmd, cmdSuffix)
+			if repoResult.Output[0] != expectedOutput {
+				t.Errorf("Unexpected result Output, expected %s, but got %s", expectedOutput, repoResult.Output[0])
+			}
 		}
 	}
 }
@@ -78,7 +115,7 @@ func TestExecGitBranchWithAFailure(t *testing.T) {
 			}
 		} else {
 			// Sample command is : git --git-dir /tmp/repos/repo1/.git --work-tree /tmp/repos/repo1 branch -av
-			expectedOutput := fmt.Sprintf("cmdName: git, args: --git-dir %s/.git --work-tree %s branch -av", repoPath, repoPath)
+			expectedOutput := fmt.Sprintf("cmdName: git, args: --git-dir %s/.git --work-tree %s %s -av", repoPath, repoPath, branchCmd)
 			if repoResult.Output[0] != expectedOutput {
 				t.Errorf("Unexpected result Output, expected %s, but got %s", expectedOutput, repoResult.Output[0])
 			}
@@ -113,129 +150,6 @@ func TestExecGitClone(t *testing.T) {
 		if repoResult.Output[0] != expectedOutput {
 			t.Errorf("Unexpected result Output, expected %s, but got %s", expectedOutput, repoResult.Output[0])
 		}
-	}
-}
-
-func TestExecGitFetch(t *testing.T) {
-	// Needed to be in sorted order as the test assertions depend on this
-	repoPaths := make([]string, 100)
-	for index := 0; index < len(repoPaths); index++ {
-		repoPaths[index] = fmt.Sprintf("/tmp/repos/repo%03d", index+1)
-	}
-	remoteName := "origin"
-
-	previous := cmdExecutionFn
-	defer func() { cmdExecutionFn = previous }()
-	cmdExecutionFn = execCmdSpy
-
-	results := execGitFetch(repoPaths, remoteName)
-
-	if len(results) != len(repoPaths) {
-		t.Errorf("Unexpected result count : %d", len(results))
-	}
-	for index, repoPath := range repoPaths {
-		repoResult := results[index] // Since we ensured the input is sorted, we can rely on the order
-
-		if repoResult.RepoPath != repoPath {
-			t.Errorf("Unexpected result RepoPath, expected %s, but got %s", repoPath, repoResult.RepoPath)
-		}
-		if repoResult.Command != fetchCmd {
-			t.Errorf("Unexpected result Command, expected %s, but got %s", fetchCmd, repoResult.Command)
-
-		}
-		// Sample command is : git --git-dir /tmp/repos/repo1/.git --work-tree /tmp/repos/repo1 fetch origin
-		expectedOutput := fmt.Sprintf("cmdName: git, args: --git-dir %s/.git --work-tree %s fetch %s", repoPath, repoPath, remoteName)
-		if repoResult.Output[0] != expectedOutput {
-			t.Errorf("Unexpected result Output, expected %s, but got %s", expectedOutput, repoResult.Output[0])
-		}
-	}
-}
-
-func TestExecGitPull(t *testing.T) {
-	repoPath := "/tmp/repos/repo1"
-	remoteName := "upstream"
-
-	previous := cmdExecutionFn
-	defer func() { cmdExecutionFn = previous }()
-	cmdExecutionFn = execCmdSpy
-
-	results := execGitPull([]string{repoPath}, remoteName)
-
-	if len(results) != 1 {
-		t.Errorf("Unexpected result count : %d", len(results))
-	}
-
-	repoResult := results[0]
-
-	if repoResult.RepoPath != repoPath {
-		t.Errorf("Unexpected result RepoPath, expected %s, but got %s", repoPath, repoResult.RepoPath)
-	}
-	if repoResult.Command != pullCmd {
-		t.Errorf("Unexpected result Command, expected %s, but got %s", pullCmd, repoResult.Command)
-
-	}
-	// Sample command is : git --git-dir /tmp/repos/repo1/.git --work-tree /tmp/repos/repo1 pull origin
-	expectedOutput := fmt.Sprintf("cmdName: git, args: --git-dir %s/.git --work-tree %s pull %s", repoPath, repoPath, remoteName)
-	if repoResult.Output[0] != expectedOutput {
-		t.Errorf("Unexpected result Output, expected %s, but got %s", expectedOutput, repoResult.Output[0])
-	}
-}
-
-func TestExecGitRemote(t *testing.T) {
-	repoPath := "/tmp/repos/repo1"
-
-	previous := cmdExecutionFn
-	defer func() { cmdExecutionFn = previous }()
-	cmdExecutionFn = execCmdSpy
-
-	results := execGitRemote([]string{repoPath})
-
-	if len(results) != 1 {
-		t.Errorf("Unexpected result count : %d", len(results))
-	}
-
-	repoResult := results[0]
-
-	if repoResult.RepoPath != repoPath {
-		t.Errorf("Unexpected result RepoPath, expected %s, but got %s", repoPath, repoResult.RepoPath)
-	}
-	if repoResult.Command != remoteCmd {
-		t.Errorf("Unexpected result Command, expected %s, but got %s", remoteCmd, repoResult.Command)
-
-	}
-	// Sample command is : git --git-dir /tmp/repos/repo1/.git --work-tree /tmp/repos/repo1 remote --verbose
-	expectedOutput := fmt.Sprintf("cmdName: git, args: --git-dir %s/.git --work-tree %s remote --verbose", repoPath, repoPath)
-	if repoResult.Output[0] != expectedOutput {
-		t.Errorf("Unexpected result Output, expected %s, but got %s", expectedOutput, repoResult.Output[0])
-	}
-}
-
-func TestExecGitStatus(t *testing.T) {
-	repoPath := "/tmp/repos/repo1"
-
-	previous := cmdExecutionFn
-	defer func() { cmdExecutionFn = previous }()
-	cmdExecutionFn = execCmdSpy
-
-	results := execGitStatus([]string{repoPath})
-
-	if len(results) != 1 {
-		t.Errorf("Unexpected result count : %d", len(results))
-	}
-
-	repoResult := results[0]
-
-	if repoResult.RepoPath != repoPath {
-		t.Errorf("Unexpected result RepoPath, expected %s, but got %s", repoPath, repoResult.RepoPath)
-	}
-	if repoResult.Command != statusCmd {
-		t.Errorf("Unexpected result Command, expected %s, but got %s", statusCmd, repoResult.Command)
-
-	}
-	// Sample command is : git --git-dir /tmp/repos/repo1/.git --work-tree /tmp/repos/repo1 status --porcelain
-	expectedOutput := fmt.Sprintf("cmdName: git, args: --git-dir %s/.git --work-tree %s status --porcelain", repoPath, repoPath)
-	if repoResult.Output[0] != expectedOutput {
-		t.Errorf("Unexpected result Output, expected %s, but got %s", expectedOutput, repoResult.Output[0])
 	}
 }
 
