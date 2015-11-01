d7b442e7196d10d20a9920ad8109eff21e545614
pmcgrath <pmcgrath@gmail.com>
2015-11-01 21:05:24 +0000

    Added env resource listing

--- rur/main.go
+++ rur/main.go
@@ -14,17 +14,47 @@ func main() {
 		log.Fatal(err)
 	}
 
+	var svcConfigs []serviceConfiguration
 	for _, subDirectoryPath := range subDirectoryPaths {
 		if !testIfFileExists(subDirectoryPath + "/dn-ci-runner.ps1") {
 			continue
 		}
 
-		configuration, err := parseForService(subDirectoryPath)
+		svcConfig, err := parseForService(subDirectoryPath)
 		if err != nil {
 			log.Fatal(err)
 		}
+		svcConfigs = append(svcConfigs, svcConfig)
 
 		fmt.Printf("\n\n*******\n")
-		fmt.Println(configuration)
+		fmt.Println(svcConfig)
+	}
+
+	envResourceMaps := make(map[string]map[string][]string)
+	for _, svcConfig := range svcConfigs {
+		for env, envConfig := range svcConfig.Environments {
+			envResourceMap, ok := envResourceMaps[env]
+			if !ok {
+				envResourceMap = make(map[string][]string)
+			}
+			for _, database := range envConfig.Databases {
+				envResourceMap[svcConfig.Name] = append(envResourceMap[svcConfig.Name], fmt.Sprintf("DB Type=%s, Host=%s, Name=%s", database.Type, database.Host, database.Name))
+			}
+			for _, logger := range envConfig.Loggers {
+				envResourceMap[svcConfig.Name] = append(envResourceMap[svcConfig.Name], fmt.Sprintf("LOGGER Destination=%s", logger.Destination))
+			}
+
+			envResourceMaps[env] = envResourceMap
+		}
+	}
+
+	for env, envResourceMap := range envResourceMaps {
+		fmt.Printf("\n\nENV: %s\n", env)
+
+		for svc, resource := range envResourceMap {
+			for _, resourceItem := range resource {
+				fmt.Printf("\t%s --> %s\n", svc, resourceItem)
+			}
+		}
 	}
 }
